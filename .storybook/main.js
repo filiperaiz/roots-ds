2393feda9fff29f64aaeccdbcb605c909fcf44c4
Filipe Raiz <filiperaiz@gmail.com>
2023-04-25 20:19:09 -0300

    chore: update rollup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
   stories: ["../src/storydocs/**/*.stories.mdx", "../src/**/stories.@(ts|tsx|js|jsx|mdx)"],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', "@storybook/addon-mdx-gfm"],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
   typescript: {
     check: true // type-check stories during Storybook build
--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,21 +6,18 @@ import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
 import typescript from '@rollup/plugin-typescript'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const packageJson = require('./package.json')
-
 export default [
   {
     input: 'src/index.ts',
     output: [
       {
-        file: packageJson.main,
+        file: 'dist/cjs/index.js',
         format: 'cjs',
         sourcemap: true,
         name: 'react-lib'
       },
       {
-        file: packageJson.module,
+        file: 'dist/esm/index.js',
         format: 'esm',
         sourcemap: true
       }
