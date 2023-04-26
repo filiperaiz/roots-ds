1d93e49ce5013402571d12251fa278f05c71c45d
Filipe Raiz <filiperaiz@gmail.com>
2023-04-25 22:53:35 -0300

    chore: update rollup terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,9 @@
 import dts from 'rollup-plugin-dts'
 import external from 'rollup-plugin-peer-deps-external'
-import { terser } from 'rollup-plugin-terser'
 
 import commonjs from '@rollup/plugin-commonjs'
 import resolve from '@rollup/plugin-node-resolve'
+import terser from '@rollup/plugin-terser'
 import typescript from '@rollup/plugin-typescript'
 
 export default [
